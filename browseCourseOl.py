a59015de50a280d56c313bd6c95e4d22d4cefef4
Luke-Evan <110676251+Luke-Evan@users.noreply.github.com>
2023-09-15 11:06:09 +0800

    Add files via upload

---
+++ browseCourseOl.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+LoginName = "13155404501"
+PWD = "rlbxxt3312"
+
+wd = webdriver.Edge()
+wd.get('http://nju.fanya.chaoxing.com/')
+wd.implicitly_wait(10)
+
+# 1. 点击登录，跳转到输入界面
+login = wd.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[4]/p/span/label/input')
+login.click()
+for handle in wd.window_handles:
+    # 先切换到该窗口
+    wd.switch_to.window(handle)
+    # 得到该窗口的标题栏字符串，判断是不是我们要操作的那个窗口
+    if '登录' in wd.title:
+        # 如果是，那么这时候WebDriver对象就是对应的该该窗口，正好，跳出循环，
+        break
+
+# 2. 输入账号密码登录
+loginName = wd.find_element(By.CSS_SELECTOR, '#phone')
+loginName.send_keys(LoginName)
+Password = wd.find_element(By.CSS_SELECTOR, '#pwd')
+Password.send_keys(PWD)
+logButton = wd.find_element(By.CSS_SELECTOR, '#phoneLoginBtn')
+logButton.click()
+
+# 3. 进入对应课程界面
+wd.switch_to.frame('frame_content')
+course = wd.find_element(By.XPATH, '//*[@id="course_226013153_79470563"]/div[2]/h3/a/span')
+course.click()
+for handle in wd.window_handles:
+    wd.switch_to.window(handle)
+    if '党的纪律建设' in wd.title:
+        break
+
+# 4. 进入前逐个检查视频任务点是否完成
+chapter = wd.find_element(By.XPATH, '//*[@id="nav_63346"]')
+chapter.click()
+wd.switch_to.frame('frame_content-zj')
+tips = wd.find_elements(By.CLASS_NAME, 'bntHoverTips')
+for tip in tips:
+    if '已完成' in tip.get_attribute('textContent'):
+        continue
+    else:
+        tipClick = tip.find_element(By.XPATH, './..')
+        tipClick.click()
+        break
+
+# 5.进入视频学习界面
+for handle in wd.window_handles:
+    wd.switch_to.window(handle)
+    if '学生学习界面' in wd.title:
+        break
+
+finished = False
+
+while not finished:
+    views = wd.find_elements(By.CLASS_NAME, 'prevHoverTips')
+    for view in views:
+        if '已完成' in view.get_attribute('textContent'):
+            continue
+        else:
+            viewClick = view.find_element(By.XPATH, './../preceding-sibling::*[1]')
+            viewClick.click()
+            # 6.开始未完成的视频
+            # 注意恶心的双重iframe
+            wd.switch_to.frame("iframe")
+            wd.switch_to.frame(wd.find_element(By.TAG_NAME, 'iframe'))
+            beginButton = wd.find_element(By.CLASS_NAME, 'vjs-big-play-button')
+            beginButton.click()
+
+            ariaValue = wd.find_element(By.XPATH, '//*[@id="video"]/div[5]/div[5]/div')
+            while True:
+                ariaNow = ariaValue.get_attribute('aria-valuenow')
+                if float(ariaNow) < 100.00:
+                    continue
+                else:
+                    wd.switch_to.default_content()
+                    wd.refresh()
+                    break
+    finished = True
---
+++ seizeCourse.py
@@ -0,0 +1,47 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+ACCOUNT = "221250170"
+PASSWORD = "rlbnjdx3312"
+
+# 创建 WebDriver 对象，指明使用chrome浏览器驱动
+wd = webdriver.Edge()
+# 打开指定网站
+wd.get("https://xk.nju.edu.cn/")
+
+wd.implicitly_wait(10)
+account = wd.find_element(By.ID, "loginName")
+account.send_keys(ACCOUNT)
+password = wd.find_element(By.ID, "loginPwd")
+password.send_keys(PASSWORD)
+input()
+titles = wd.find_elements(By.CSS_SELECTOR, "ul#cvPageHeadTab li")
+for title in titles:
+    if "收藏" in title.get_attribute('outerHTML'):
+        title.click()
+while True:
+    finish = True
+    while finish:
+        sleep(0.5)
+        wd.refresh()
+        courses = wd.find_elements(By.CSS_SELECTOR, "div table tbody tr")
+        for course in courses:
+            try:
+                if (("已满" not in course.get_attribute("outerHTML"))
+                        and "仙林校区" in course.get_attribute("outerHTML")):
+                    # and ("乒乓球" in course.get_attribute("outerHTML") or "羽毛球" in course.get_attribute("outerHTML"))
+                    star = course.find_element(By.CSS_SELECTOR, "a.cv-choice")
+                    wd.execute_script("arguments[0].scrollIntoView();", course)
+                    star.click()
+                    finish = False
+                    break
+            except:
+                break
+        print(1)
+    make_sure = wd.find_element(By.CSS_SELECTOR, "div#cvDialog  div.cv-foot div.cv-sure")
+    make_sure.click()
+    t = input()
+    if t == 1:
+        break
